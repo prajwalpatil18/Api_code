1aef93aeb3ace3c814a4004231469845c7bc0586
prajwalpatil18 <92904433+prajwalpatil18@users.noreply.github.com>
2025-11-29 23:42:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -388,3 +388,10 @@ def chat(payload: ChatIn):
     db.close()
 
     return {"answer": answer, "conversation_id": conv_id}
+
+
+import uvicorn
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run (app,host ="0.0.0.0", port=port)
